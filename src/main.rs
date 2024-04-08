e0e63e1536b30f84201d960b54023da63800f8f4
jonny <johnnybarda423@gmail.com>
2024-04-08 19:15:31 +0300

    pass 3st stage

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,106 @@
-use std::net::TcpListener;
+use std::io::{BufRead, BufReader, Read, Write};
+use std::net::{TcpListener};
+use anyhow::{Context, Result};
+use crate::Command::PING;
 
-fn main() {
+
+#[derive(Debug, Ord, PartialOrd, PartialEq, Eq)]
+enum Command {
+    PING,
+    // Set { key: String, value: String },
+    // Get { key: String },
+}
+
+fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    let address = "127.0.0.1:6379";
+    let listener = TcpListener::bind(address).with_context(
+        || format!("tcp bind to {address}")
+    )?;
+    println!("listening to {address}");
 
-
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let mut reader = BufReader::new(&stream);
+                let mut data = String::new();
+                reader.read_to_string(&mut data).unwrap();
+                let commands: Vec<Command> = parse_commands(&data);
+                for command in commands {
+                    match command {
+                        PING => { stream.write_all(b"+PONG\r\n")?; }
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+    Ok(())
+}
+
+fn parse_commands(data: &str) -> Vec<Command> {
+    tokenize(&data).iter()
+        .filter_map(
+            |t| parse_command(t)
+        )
+        .collect()
+}
+
+
+fn tokenize(input: &str) -> Vec<Vec<&str>> {
+    let mut lines = input.lines();
+    let mut result = vec![];
+    while let Some(value) = lines.next() {
+        if value.is_empty() {
+            break;
+        }
+        let length: usize = value[1..].parse().unwrap();
+        let mut command = vec![];
+        for _ in 0..length {
+            let x = lines.next().unwrap();
+            println!("tokenizing {x}");
+
+            let val = lines.next().unwrap();
+            println!("tokenizing {val}");
+            command.push(val);
+        }
+        result.push(command);
+    }
+    return result;
+}
+
+fn parse_command(input: &Vec<&str>) -> Option<Command> {
+    match input.as_slice() {
+        ["PING"] => { Some(PING) }
+        _ => None,
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_ping() {
+        let input = "*1\r\n$4\r\nPING\r\n";
+        assert_eq!(tokenize(input), vec![vec!["PING"]]);
+    }
+
+    #[test]
+    fn test_parse_ping() {
+        let input = "*1\r\n$4\r\nPING\r\n";
+        assert_eq!(parse_commands(input), vec![PING]);
+    }
+
+    #[test]
+    fn test_full() {
+        let input = "*3\r\n$3\r\nset\r\n$3\r\nfoo\r\n$3\r\nbar\r\n*2\r\n$3\r\nget\r\n$3\r\nfoo\r\n";
+        assert_eq!(tokenize(input), vec![
+            vec!["set", "foo", "bar"],
+            vec!["get", "foo"],
+        ]);
     }
 }
