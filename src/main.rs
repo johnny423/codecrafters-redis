01a501cb5a3233e5eaff3b8a1026292504f7cf62
jonny <johnnybarda423@gmail.com>
2024-04-08 20:02:10 +0300

    pass 5st stage

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,56 @@
-use std::io::{BufReader, Read, Write};
-use std::net::{TcpListener};
-use anyhow::{Context, Result};
-use crate::Command::PING;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 
 #[derive(Debug, Ord, PartialOrd, PartialEq, Eq)]
-enum Command {
+pub enum Command {
     PING,
     // Set { key: String, value: String },
     // Get { key: String },
 }
 
-fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    let address = "127.0.0.1:6379";
-    let listener = TcpListener::bind(address).with_context(
-        || format!("tcp bind to {address}")
-    )?;
-    println!("listening to {address}");
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Server listening on port 6379");
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                loop {
-                    println!("got new stream");
-                    let mut reader = BufReader::new(&stream);
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(async move {
+            handle_client(stream).await;
+        });
+    }
+}
 
-                    let mut buf = [0; 512];
-                    let n = reader.read(&mut buf).unwrap();
-                    if n == 0 {
-                        break;
-                    }
-                    println!("got {n} bytes {buf:?}");
-                    let data = String::from_utf8(buf.to_vec())?;
+async fn handle_client(mut stream: TcpStream) {
+    println!("Client connected: {}", stream.peer_addr().unwrap());
 
-                    println!("got {data:?}");
-                    let commands: Vec<Command> = parse_commands(&data.trim_end_matches('\0'));
-                    for command in commands {
-                        match command {
-                            PING => { stream.write_all(b"+PONG\r\n")?; }
+    let (mut reader, mut writer) = stream.split();
+
+    let mut buf = [0; 1024];
+    loop {
+        match reader.read(&mut buf).await {
+            Ok(0) => {
+                println!("Client disconnected: {}", stream.peer_addr().unwrap());
+                return;
+            }
+            Ok(n) => {
+                let data: String = String::from_utf8(buf.to_vec()).unwrap();
+                let data = data.trim_end_matches('\0');
+                for command in parse_commands(data) {
+                    match command {
+                        Command::PING => {
+                            writer.write_all(b"+PONG\r\n").await.unwrap();
                         }
                     }
                 }
             }
-            Err(e) => {
-                println!("error: {}", e);
+            Err(err) => {
+                eprintln!("Error reading from socket: {}", err);
+                return;
             }
         }
     }
-    Ok(())
 }
 
 fn parse_commands(data: &str) -> Vec<Command> {
@@ -86,7 +87,7 @@ fn tokenize(input: &str) -> Vec<Vec<&str>> {
 
 fn parse_command(input: &Vec<&str>) -> Option<Command> {
     match input.as_slice() {
-        ["ping"] | ["PING"] => { Some(PING) }
+        ["ping"] | ["PING"] => { Some(Command::PING) }
         _ => None,
     }
 }
@@ -104,7 +105,7 @@ mod test {
     #[test]
     fn test_parse_ping() {
         let input = "*1\r\n$4\r\nPING\r\n";
-        assert_eq!(parse_commands(input), vec![PING]);
+        assert_eq!(parse_commands(input), vec![Command::PING]);
     }
 
     #[test]
@@ -116,3 +117,96 @@ mod test {
         ]);
     }
 }
+
+
+//         println!("got new stream");
+//         let mut reader = BufReader::new(&reader);
+//
+//         let mut buf = [0; 512];
+//         let n = reader.read(&mut buf).unwrap();
+//         if n == 0 {
+//             break;
+//         }
+//         println!("got {n} bytes {buf:?}");
+//         let data = String::from_utf8(buf.to_vec())?;
+//
+//         println!("got {data:?}");
+//         let commands: Vec<Command> = parse_commands(&data.trim_end_matches('\0'));
+//         for command in commands {
+//             match command {
+//                 PING => { stream.write_all(b"+PONG\r\n")?; }
+//             }
+//         }
+//     }
+// }
+// );
+//     println!("Client connected: {}", stream.peer_addr().unwrap());
+//
+//     let (mut reader, mut writer) = stream.split();
+//
+//     // Echo messages back to the client
+//     let mut buf = [0; 1024];
+//     loop {
+//         match reader.read(&mut buf).await {
+//             Ok(0) => {
+//                 println!("Client disconnected: {}", stream.peer_addr().unwrap());
+//                 return;
+//             }
+//             Ok(n) => {
+//                 if let Err(err) = writer.write_all(&buf[..n]).await {
+//                     eprintln!("Error writing to socket: {}", err);
+//                     return;
+//                 }
+//             }
+//             Err(err) => {
+//                 eprintln!("Error reading from socket: {}", err);
+//                 return;
+//             }
+//         }
+//     }
+// }
+//
+// fn main() -> Result<()> {
+//     // You can use print statements as follows for debugging, they'll be visible when running tests.
+//     let address = "127.0.0.1:6379";
+//     let listener = TcpListener::bind(address).with_context(
+//         || format!("tcp bind to {address}")
+//     )?;
+//     println!("listening to {address}");
+//
+//
+//     for stream in listener.incoming() {
+//         match stream {
+//             Ok(mut stream) => {
+//                 thread::spawn(move ||
+//                     {
+//                         loop {
+//                             println!("got new stream");
+//                             let mut reader = BufReader::new(&stream);
+//
+//                             let mut buf = [0; 512];
+//                             let n = reader.read(&mut buf).unwrap();
+//                             if n == 0 {
+//                                 break;
+//                             }
+//                             println!("got {n} bytes {buf:?}");
+//                             let data = String::from_utf8(buf.to_vec())?;
+//
+//                             println!("got {data:?}");
+//                             let commands: Vec<Command> = parse_commands(&data.trim_end_matches('\0'));
+//                             for command in commands {
+//                                 match command {
+//                                     PING => { stream.write_all(b"+PONG\r\n")?; }
+//                                 }
+//                             }
+//                         }
+//                     }
+//                 );
+//             }
+//             Err(e) => {
+//                 println!("error: {}", e);
+//             }
+//         }
+//     }
+//     Ok(())
+// }
